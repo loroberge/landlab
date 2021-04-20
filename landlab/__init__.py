6bb8b2a5591c70891f346da06b449fd82326bf39
BCampforts <benjamincampforts@gmail.com>
2021-04-20 09:56:32 -0600

    add imshowhs to plot DEMs with hillshades and support multiple overlays over hillshades

--- landlab/__init__.py
+++ landlab/__init__.py
@@ -30,7 +30,7 @@ from .grid import (
 )
 from .grid.linkstatus import LinkStatus
 from .grid.nodestatus import NodeStatus
-from .plot import imshow_grid, imshow_grid_at_node
+from .plot import imshow_grid, imshowhs_grid,  imshow_grid_at_node
 
 try:
     set_printoptions(legacy="1.13")
@@ -59,7 +59,8 @@ __all__ = [
     "LinkStatus",
     "NodeStatus",
     "create_grid",
-    "imshow_grid",
+    "imshow_grid",    
+    "imshowhs_grid",
     "imshow_grid_at_node",
 ]
 
