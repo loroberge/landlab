58aad29ecf7011438fe202e6d65e86ca01ebb312
BCampforts <benjamincampforts@gmail.com>
2021-04-23 18:39:42 -0600

    adjust init files

--- landlab/components/__init__.py
+++ landlab/components/__init__.py
@@ -21,7 +21,7 @@ from .fracture_grid import FractureGridGenerator
 from .gflex import gFlex
 from .groundwater import GroundwaterDupuitPercolator
 from .hack_calculator import HackCalculator
-from .hand_calculator import HeightAboveDrainageCalculator
+from .hylands import Hylands
 from .lake_fill import LakeMapperBarnes
 from .landslides import LandslideProbability
 from .lateral_erosion import LateralEroder
@@ -90,6 +90,7 @@ COMPONENTS = [
     GroundwaterDupuitPercolator,
     HackCalculator,
     HeightAboveDrainageCalculator,
+    Hylands,
     KinwaveImplicitOverlandFlow,
     KinwaveOverlandFlowModel,
     LakeMapperBarnes,
